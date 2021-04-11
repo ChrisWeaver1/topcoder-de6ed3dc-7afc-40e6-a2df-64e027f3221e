6b3d300f77da283d910ea76ef295280d04f6f216
chrisweaver1 <chris.weaver.work@gmail.com>
2021-04-11 09:59:13 +0000

    comments and tidying up abit

--- src/basic_auth_middleware.rs
+++ src/basic_auth_middleware.rs
@@ -4,9 +4,7 @@ use rocket::{
     outcome::IntoOutcome,
     http::Status,
     request::FromRequest,
-    self, 
-    Response, 
-    Responder
+    self
 };
 extern crate base64;
 
@@ -15,43 +13,72 @@ pub struct BasicAuth {
     pub password: String,
 }
 
+// implement FromRequest from rocket on our BasicAuth struct
 impl<'a, 'r> FromRequest<'a, 'r> for BasicAuth {
     type Error = ();
 
     fn from_request(request: &Request) -> Outcome<Self, (Status, <Self as FromRequest<'a, 'r>>::Error), ()> {
+        
+        // get Authorization header value
+        // should look something like "basic owroioerw=="
         let auth_header = request.headers().get_one("Authorization");
+
+        // Unwrap & check option value
         if let Some(auth_header) = auth_header {
-            let split = auth_header.split_whitespace().collect::<Vec<_>>();
+
+            // split on whitespace, collect into array
+            // type was originally Vec<_>, look into what that means, 
+            // I imagine its just a way of taking the value of split_whitespace
+            let split = auth_header.split_whitespace().collect::<Vec<&str>>();
+    
+            // check length of split value is 2
             if split.len() != 2 {
                 return Outcome::Failure((Status::Unauthorized, ()));
             }
+    
+            // set values as variables
             let (basic, payload) = (split[0], split[1]);
+            // check header uses basic auth
             if basic != "Basic" {
                 return Outcome::Failure((Status::Unauthorized, ()));
             }
+    
+            // decode base64 value, seems to decode into byte array
             let decoded = base64::decode(payload)
                 .ok()
-                .into_outcome((Status::BadRequest, ()))?;
-
+                .into_outcome((Status::Unauthorized, ()))?;
+    
+            // create a string from decoded array
             let decoded_str = String::from_utf8(decoded)
                 .ok()
-                .into_outcome((Status::BadRequest, ()))?;
-
+                .into_outcome((Status::Unauthorized, ()))?;
+    
+            // split username:password
             let split = decoded_str.split(":").collect::<Vec<_>>();
-
-            // If exactly username & password pair are present
+    
+            // check array value is expected length
             if split.len() != 2 {
-                return Outcome::Failure((Status::BadRequest, ()));
+                return Outcome::Failure((Status::Unauthorized, ()));
             }
-
+            
+            // set username and password variables
             let (username, password) = (split[0].to_string(), split[1].to_string());
-
-            Outcome::Success(BasicAuth {
-                username,
-                password
-            })
-        } else {
-            Outcome::Failure((Status::Unauthorized, ()))
+    
+            // check username, password values
+            // return success if correct, unauthorized otherwise 
+            if username == "topcoder" && password == "rocks" {
+                return Outcome::Success(BasicAuth {
+                    username,
+                    password
+                })
+            }
+            else {
+                return Outcome::Failure((Status::Unauthorized, ()));
+            }
+        }
+        else {
+            // No authorization header value
+            return Outcome::Failure((Status::Unauthorized, ()))
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,32 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
-pub extern crate tera;
-use rocket_contrib::templates::Template;
-use std::collections::HashMap;
+//pub extern crate tera;
+
 mod basic_auth_middleware;
 
-use rocket::response::status::Custom;
-use rocket::{Catcher, Request, Response, catch};
-use rocket::response::{Result, Responder};
+use rocket_contrib::templates::Template;
+use std::collections::HashMap;
+use rocket::{Request, Response, catch};
+use rocket::response::{Result};
 use rocket::http::{Header, Status};
 
 #[get("/")]
 fn index(auth: basic_auth_middleware::BasicAuth) -> Template {
     let mut context: HashMap<&str, &str> = HashMap::new();
-    context.insert("name", "chirs");
+    context.insert("name", &auth.username);
     return Template::render("index", context);
 }
 
 #[catch(401)]
-fn basic_auth<'a>(req: &Request) -> Result<'a> {
+fn basic_auth<'a>(_req: &Request) -> Result<'a> {
+    // build a respose
     return Response::build()
-        .header(Header::new("WWW-Authenticate", "Basic realm=\"realm\""))
+        // add header
+        .header(Header::new("WWW-Authenticate", "Basic realm=\"topcoder-challenge\""))
+        // set status code
         .status(Status::Unauthorized)
+        // wrap response as result
         .ok();
 }
 
